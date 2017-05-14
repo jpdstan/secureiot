04f81dc457d032a06e2ff4e20b3f63c7fbe95b5b
Stanley Kwong <jpdstan@gmail.com>
2017-05-15 05:31:48 +0800

    change mac to ip for simplification

--- secure_server.py
+++ secure_server.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 
-host, port = "127.0.0.1", 8080
+host, port = "192.168.1.38", 8080
 
 # Caches users (MAC addresses) and their public keys in memory to minimize DB queries.
 cached_users = {}
@@ -12,10 +12,10 @@ mongo_client = MongoClient('localhost', 27017)
 db = mongo_client['key_database']
 
 # Register the client with MAC_ADDR and their PUB KEY.
-def register_client(mac_addr, pub_key):
-	print("Registering " + mac_addr + " with " + pub_key)
+def register_client(ip_addr, pub_key):
+	print("Registering " + ip_addr + " with " + pub_key)
 	db.posts.insert_one({
-		'mac_addr': mac_addr,
+		'ip_addr': ip_addr,
 		'pub_key' : pub_key})
 	
 	# results = db.posts.find()
@@ -24,8 +24,9 @@ def register_client(mac_addr, pub_key):
 	# 	print(result)
 
 # Get the public key of MAC_ADDR.
-def get_pub_key(mac_addr):
-	entry = db.posts.find_one({'mac_addr': mac_addr})
+def get_pub_key(ip_addr): # todo need to make sure this correct signature
+
+	entry = db.posts.find_one({'ip_addr': ip_addr})
 	if entry:
 		print(entry)
 		return entry['pub_key']
@@ -35,10 +36,13 @@ class RequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		parsed = urlparse(self.path)
 		if parsed.path == '/user_pk':
-			pub_key = get_pub_key(parsed.query)
+			pub_key = get_pub_key(parsed.query[2:])
 			if not pub_key is None:
 				self.send_response(200, pub_key)
+				self.end_headers()
+				pass
 		self.send_response(400)
+		self.end_headers()
 
 # Listen for incoming HTTP requests.
 def listen():
